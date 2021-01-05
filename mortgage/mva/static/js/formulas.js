534841fc6bf13fae90b0c725134ed03254c49279
VladicUn <anvladic13@gmail.com>
2021-01-05 23:15:06 +0300

    svg panelkas background

--- mortgage/mva/static/js/formulas.js
+++ mortgage/mva/static/js/formulas.js
@@ -11,21 +11,18 @@ function mortgage(income, mortgage_sum, rate, term, spendings) {
       if(a==0) cumsum_income[a] = income;
       else cumsum_income[a] = cumsum_income[a-1] + income;
     }
-    console.log(cumsum_income)
 
     cumsum_annual_pay = [];
     for(var a=0;a<term*12;a++) {
       if(a==0) cumsum_annual_pay[a] = annual_pay;
       else cumsum_annual_pay[a] = cumsum_annual_pay[a-1] + annual_pay;
     }
-    console.log(cumsum_annual_pay)
 
     cumsum_spendings = [];
     for(var a=0;a<term*12;a++) {
       if(a==0) cumsum_spendings[a] = spendings;
       else cumsum_spendings[a] = cumsum_spendings[a-1] + spendings;
     }
-    console.log(cumsum_spendings)
 
     var cumsum_profit = cumsum_income.map(function(item, index) {
       return item - cumsum_annual_pay[index];})
@@ -33,7 +30,6 @@ function mortgage(income, mortgage_sum, rate, term, spendings) {
     var cumsum_profit_final = cumsum_profit.map(function(item, index) {
       return item - cumsum_spendings[index];})
 
-    console.log(cumsum_profit_final)
 
     return cumsum_profit_final
 }
@@ -47,21 +43,16 @@ function accumulation(income, apt_sum, inflation_rate, term, spendings) {
       if(a==0) cumsum_accum[a] = accum/(term*12);
       else cumsum_accum[a] = cumsum_accum[a-1] + accum/(term*12);
     }
-    console.log(cumsum_accum)
 
     cumsum_FV = [];
     for(var a=0;a<term*12;a++) {
       if(a==0) cumsum_FV[a] = (apt_sum*(1+inflation_rate/12) - apt_sum);
       else cumsum_FV[a] = cumsum_FV[a-1] + (apt_sum*(1+inflation_rate/12)-apt_sum);
     }
-    console.log(cumsum_FV)
 
     var cumsum_profit_accum = cumsum_accum.map(function(item, index) {
           return item - cumsum_FV[index];})
-    console.log(cumsum_profit_accum)
 
     return  cumsum_profit_accum
 }
 
-console.log(mortgage(100000, 8000000, 0.07, 10, 5000))
-console.log(accumulation(140000, 8000000, 0.05, 10, 25000))
