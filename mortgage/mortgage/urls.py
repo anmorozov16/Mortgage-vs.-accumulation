ecfa0650a5c4cf8cee8c4ec68b40e5b1ca24c171
VladicUn <anvladic13@gmail.com>
2021-01-05 14:45:57 +0300

    setup settings

--- mortgage/mortgage/urls.py
+++ mortgage/mortgage/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import mva.views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', v.main)
 ]
